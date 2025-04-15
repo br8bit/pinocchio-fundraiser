cc8c3d2381446ea9dda1a16aadeaf074a78b162f
br8bit <>
2025-04-15 12:49:16 +0400

    add contributor state

--- src/state/contributor.rs
+++ src/state/contributor.rs
@@ -1,5 +1,35 @@
+use pinocchio::pubkey::{Pubkey, find_program_address};
+
 #[repr(C)]
+#[derive(Clone, Copy, Debug, Default, PartialEq)]
 pub struct Contributor {
+    pub is_initialized: bool,
     pub amount: u64,
-    pub bump: u8,
+}
+
+impl Contributor {
+    pub const CONTRIBUTOR_SEED: &[u8] = b"contributor";
+    pub const LEN: usize = core::mem::size_of::<Self>();
+
+    #[inline(always)]
+    pub fn is_initialized(&self) -> bool {
+        self.is_initialized
+    }
+
+    #[inline(always)]
+    pub fn initialize(
+        &mut self,
+        Contributor {
+            amount,
+            is_initialized,
+        }: Contributor,
+    ) {
+        self.is_initialized = true;
+        self.amount = amount;
+    }
+
+    #[inline(always)]
+    pub fn update(&mut self, amount: u64) {
+        self.amount = amount;
+    }
 }
