5e78498159d1fb63e6db41025db28e8c302d24d2
br8bit <>
2025-04-15 12:58:36 +0400

    module set up

---
+++ src/instructions/mod.rs
@@ -0,0 +1,12 @@
+
+pub mod checker;
+pub mod contribute;
+pub mod initialize;
+pub mod program_instruction;
+pub mod refund;
+
+pub use checker::*;
+pub use contribute::*;
+pub use initialize::*;
+pub use program_instruction::*;
+pub use refund::*;
